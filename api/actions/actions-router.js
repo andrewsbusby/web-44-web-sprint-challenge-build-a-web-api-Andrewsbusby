4dd05ab6087a96d6b2625239d0778bcad61f22f5
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-06 10:18:23 -0600

    all action routers are set up

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -16,8 +16,44 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
-router.get('/:id', validateActionId, (req, res, next) => {
+router.get('/:id', validateActionId, (req, res, next) => { //eslint-disable-line
     res.json(req.action)
+})
+
+router.post('/', validateAction, (req, res, next) => {
+    Action.insert(req.body)
+        .then(newAction => {
+            res.status(201).json(newAction)
+        })
+        .catch(next)
+})
+
+router.put('/:id', validateActionId, validateAction, (req, res, next) => {
+    Action.update(req.params.id, req.body)
+        .then(() =>{
+            Action.get(req.params.id)
+        })
+        .then(updated => {
+            res.json(updated)
+        })
+        .catch(next)
+})
+
+router.delete('/:id', validateActionId, async (req, res, next) =>{
+    try{
+        await Action.remove(req.params.id)
+        res.json(req.action)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        err: err.message,
+        stack: err.stack
+    })
 })
 
 module.exports = router;
